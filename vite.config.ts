82c5af172a87800ae11756c9dbc25ddad9486767
Sergei Borja <sergei.borja0107@gmail.com>
2024-09-19 16:51:30 -0600

    Finished final details.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [ react()],
-  // base: '/maker-pre-launch-landing-page/'
+  base: '/maker-pre-launch-landing-page/'
 })
